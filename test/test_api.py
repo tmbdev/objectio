cfaccecc97efe2f4e09248dbdf27a86a036c618a
Tom <tmbdev@gmail.com>
2020-02-19 21:21:31 -0800

    removed dependency on gs: in test cases

--- test/test_api.py
+++ test/test_api.py
@@ -15,14 +15,15 @@ import io
 
 testcases = yaml.load(io.StringIO("""
 read:
-  - url: gs://lpr-openimages/openimages-shard.ipynb
+  - url: https://lpr-openimages.storage.googleapis.com/openimages-shard.ipynb
     contains: with wds.ShardWriter
 list:
-  - url: gs://lpr-openimages
-    contains: openimages-shard.ipynb
+  - url: file:/etc
+    contains: passwd
 write:
   - bucket: file:/tmp
 """), Loader=yaml.FullLoader)
+
 
 def test_objopen_read():
     for case in testcases["read"]:
@@ -31,23 +32,26 @@ def test_objopen_read():
             text = stream.read().decode("utf-8")
         assert case["contains"] in text
 
+
 def test_objopen_list():
     for case in testcases["list"]:
-      print("# testing", case["url"], file=sys.stderr)
-      with objio.objopen(case["url"], "list") as stream:
-          text = stream.read().decode("utf-8")
-      assert case["contains"] in text
+        print("# testing", case["url"], file=sys.stderr)
+        with objio.objopen(case["url"], "list") as stream:
+            text = stream.read().decode("utf-8")
+        assert case["contains"] in text
+
 
 def test_objopen_write():
     original = "hello world"
     for case in testcases["write"]:
-      fname = case["bucket"] + "/test.txt"
-      print("# testing", fname, file=sys.stderr)
-      with objio.objopen(fname, "write") as stream:
-          stream.write(original.encode("utf-8"))
-      with objio.objopen(fname, "read") as stream:
-          text = stream.read().decode("utf-8")
-      assert text == original
+        fname = case["bucket"] + "/test.txt"
+        print("# testing", fname, file=sys.stderr)
+        with objio.objopen(fname, "write") as stream:
+            stream.write(original.encode("utf-8"))
+        with objio.objopen(fname, "read") as stream:
+            text = stream.read().decode("utf-8")
+        assert text == original
+
 
 def test_gopen_file(tmp_path):
     fname = str(tmp_path / "test.txt")
@@ -57,5 +61,3 @@ def test_gopen_file(tmp_path):
     with objio.gopen(fname, "r") as stream:
         text = stream.read()
     assert text == original
-        
-
